91c5845dc6d5368b3dca0f1555a6e5d5ed5053f7
feelonewong <feelonewong@gmail.com>
2021-05-05 16:50:53 +0800

    v-model在Vue3.0的写法

--- src/components/Switch.vue
+++ src/components/Switch.vue
@@ -1,15 +1,20 @@
 <template>
     <div>
-        
-        <Switch/>
+        <Switch v-model:value="value" />
+    <div>{{value}}</div>
+
     </div>
 </template>
 
 <script lang="ts">
+import { ref } from '@vue/reactivity';
 import Switch from "../lib/Switch/Switch.vue";
 export default {
     setup() {
-        
+        const value = ref(false);
+        return{
+            value
+        }
     },
     components:{
         Switch
--- src/lib/Switch/Switch.vue
+++ src/lib/Switch/Switch.vue
@@ -1,40 +1,58 @@
 <template>
-    <div>
-        <button><span></span></button>
-    </div>
+  <div>
+    <button :class="{checked: value}" @click="toggle">
+        <span></span>
+    </button>
+  </div>
 </template>
 
 
 <script lang="ts">
+import { ref } from "@vue/reactivity";
 export default {
-    setup() {
-        
-    },
-}
+  props:{
+      value: Boolean
+  },  
+  setup(props, context) {
+    const toggle = ()=>{
+        context.emit("update:value", !props.value);
+    }
+    return {
+      toggle
+    };
+  },
+};
 </script>
 
-<style scoped lang="scss"> 
-    $h: 22px;
-    $h2: $h - 4px;
-    button{
-        height: $h;
-        width: $h*2;
-        border: none;
-        background: blue;
-        border-radius: $h/2;
-        position: relative;
-    }
-    span{
-        position: absolute;
-        top: 2px;
-        left: 2px;
-        height: $h2;
-        width: $h2;
-        background: #fff;
-        border-radius: $h2 / 2;
-    }
-    button:hover > span {
-        background: red;
-        left: calc(100% - #{$h2} - 2px );
-    }
+<style scoped lang="scss">
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: gray;
+  border-radius: $h/2;
+  position: relative;
+}
+span {
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  height: $h2;
+  width: $h2;
+  background: #fff;
+  border-radius: $h2 / 2;
+  transition: left 300ms;
+}
+button.checked{
+    background: #409eff;
+}
+button.checked > span {
+  left: calc(100% - #{$h2} - 2px);
+}
+
+button:focus {
+    outline: none;
+}
 </style>
