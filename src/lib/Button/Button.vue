e945d2555997de480c2c97905b54556cb3ecf973
feelonewong <feelonewong@gmail.com>
2021-05-05 21:02:28 +0800

    支持theme属性

--- src/lib/Button/Button.vue
+++ src/lib/Button/Button.vue
@@ -62,5 +62,26 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+  &.fl-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+  &::-moz-focus-inner {
+    border: 0;
+  }
+  &.fl-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
 }
 </style>
