c06279e207be43d6644e20bafd525d0540cad037
feelonewong <feelonewong@gmail.com>
2021-05-05 21:44:02 +0800

    支持size 属性 支持level属性

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
     <div>Button示例</div>
-    <h2>示例1</h2>
+    <h2>示例1:支持各种主题的button</h2>
     <!-- 默认支持@click事件 -->
-    <Button @click="ButtonClick" @mouseover="ButtonMouseOver"> 默认按钮 </Button>
+    <Button @click="ButtonClick" @mouseover="ButtonMouseOver">
+      默认按钮
+    </Button>
     <Button theme="button"> 普通按钮 </Button>
 
     <Button theme="text"> text按钮 </Button>
     <Button theme="link"> link按钮 </Button>
+    <h2>示例2:支持各种大小的button</h2>
+    <!-- 默认支持@click事件 -->
+    <Button size="small"> small按钮 </Button>
+    <Button> 普通按钮 </Button>
+    <Button size="big"> big按钮 </Button>
+    <h2>示例3:支持(主要、完成、危险)类型的button</h2>
+    <!-- 默认支持@click事件 -->
+    <Button size="small" level="main"> 主要链接按钮 </Button>
+    <Button> 普通按钮 </Button>
+    <Button size="big" level="danger"> 危险文字按钮 </Button>
   </div>
 </template>
 
--- src/lib/Button/Button.vue
+++ src/lib/Button/Button.vue
@@ -1,10 +1,11 @@
 <template>
-  <button v-bind="rest" class="fl-button" :class="`fl-theme-${theme}`">
+  <button v-bind="rest" class="fl-button" :class="classes">
     <slot />
   </button>
 </template>
 
 <script lang="ts">
+import { computed } from "@vue/runtime-core";
 export default {
   //不让Vue继承原生的事件
   inheritAttrs: false,
@@ -15,13 +16,37 @@ export default {
         return "button";
       },
     },
+    size: {
+      type: String,
+      default() {
+        return "normal";
+      },
+    },
+    level: {
+      type: String,
+      default() {
+        return "";
+      },
+    },
   },
   setup(props, context) {
     //   const {size, onClick, mouseover} = context.attrs的缩写;
-    const { size, ...rest } = context.attrs;
+    const { ...rest } = context.attrs;
+
+    const { theme }: any = { ...props };
+    const { size }: any = { ...props };
+    const { level }: any = { ...props };
+
+    const classes = computed(() => {
+      return {
+        [`fl-theme-${theme}`]: theme,
+        [`fl-size-${size}`]: size,
+        [`fl-level-${level}`]: level,
+      };
+    });
     return {
-      size,
       rest,
+      classes,
     };
   },
 };
@@ -33,6 +58,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .fl-button {
   box-sizing: border-box;
   height: $h;
@@ -47,6 +73,37 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  &.fl-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.fl-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+  &.fl-level-main {
+    background: $blue;
+    color: white;
+    border-color: $blue;
+    &:hover,
+    &:focus {
+      background: darken($blue, 10%);
+      border-color: darken($blue, 10%);
+    }
+  }
+  &.fl-level-danger {
+    background: $red;
+    border-color: $red;
+    color: white;
+    &:hover,
+    &:focus {
+        color: #ddd;
+      background: darken($red, 10%);
+      border-color: darken($red, 10%);
+    }
+  }
   & + & {
     margin-left: 8px;
   }
